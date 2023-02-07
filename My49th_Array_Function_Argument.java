c0e7526e63c56cc8c6e136c47b2d3c72fe9a2b79
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-02-07 20:29:45 +0530

    Add files via upload

--- My49th_Array_Function_Argument.java
+++ My49th_Array_Function_Argument.java
@@ -1,3 +1,27 @@
 public class My49th_Array_Function_Argument {
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+1;
+            }
+        }
     
+
+    public static void main(String[] args){
+        int marks[]={80,20,10};
+
+        // System.out.println("Before:- ");
+
+        // for(int i=0;i<=marks.length;i++){
+        //     System.out.println(marks[i]);
+        // }
+
+        update(marks);
+
+        System.out.println("After Updation:- ");
+        
+        for(int i=0;i<=marks.length;i++){
+            System.out.println(marks[i]);
+        }
+
+    }
 }
