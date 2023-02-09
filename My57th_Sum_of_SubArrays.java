9d6a8479f14e0b70011f9303a672c7fa94497097
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-02-09 23:25:11 +0530

    Add files via upload

--- My57th_Sum_of_SubArrays.java
+++ My57th_Sum_of_SubArrays.java
@@ -1,3 +1,28 @@
 public class My57th_Sum_of_SubArrays {
+    public static void subarraysum(int arr[]){
+        int maxsum=Integer.MIN_VALUE;
+        int cursum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                cursum=0;
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k]+"=");
+                    cursum=cursum+arr[k];
+                    if(cursum>maxsum){
+                        maxsum=cursum;
+                    }
+                    System.out.print(cursum);
+                }
+                System.out.println();
+            }
+            // System.out.println(maxsum);
+            
+        }
+        System.out.println(maxsum);
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8};
 
+        subarraysum(arr);
+    }
 }
