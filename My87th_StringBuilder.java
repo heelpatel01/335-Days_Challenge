fc254d4e37b7d1bea0d30e29d836e347e399cbff
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-02-21 23:56:57 +0530

    Add files via upload

---
+++ My87th_StringBuilder.java
@@ -0,0 +1,9 @@
+public class My87th_StringBuilder {
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder();
+        for(char ch='a';ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
