4d45a633d91a26d6dbc8fdb55e35b0d80767385f
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-05-04 23:22:10 +0530

    Update 1Array.js

--- Java Script/1Array.js
+++ Java Script/1Array.js
@@ -1,7 +1,3 @@
 var nameBillioners=["Elon Musk","Ratan Tata","Bill Gates","Dipendra Sinh Mahida","Heel Patel"]
 
 console.log(nameBillioners.includes("Heel Patel"));
-
-Jay Shree Ram
-Om Namah Shivay
-Ram Ram Ji
