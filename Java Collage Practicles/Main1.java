d5c5bc79a9d57123f62fa8594bad45d1b86b8b99
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-05-12 22:34:59 +0530

    Update Main1.java

--- Java Collage Practicles/Main1.java
+++ Java Collage Practicles/Main1.java
@@ -1,4 +1,5 @@
-// Write a program to print Welcome to Java...
+// Write a program to print Welcome to Java
+
 public class Main1{
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
